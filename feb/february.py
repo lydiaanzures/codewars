144895c9a7bb4a24badbeff9f3aeff43037ad7eb
Lydia Anzures <lydiaanzures@gmail.com>
2023-02-22 20:48:47 -0500

    boolean function

--- feb/february.py
+++ feb/february.py
@@ -5,3 +5,11 @@ def bool_to_word(boolean):
     else:
         print("No")
         return("No")
+
+def bool_to_word(boolean):
+    if boolean == True:
+        print("Yes")
+        return("Yes")
+    else:
+        print("No")
+        return("No")
